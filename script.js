2487e431eb62cf8cdb2787852fa54a7dd80259f0
Aditi15042006 <aditisahu152006@gmail.com>
2025-11-30 11:30:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+function addColor() {
+    const color = document.getElementById("colorPicker").value;
+    const palette = document.getElementById("palette");
+
+    const box = document.createElement("div");
+    box.className = "color-box";
+    box.style.backgroundColor = color;
+
+    palette.appendChild(box);
+}
 
